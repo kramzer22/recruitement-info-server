36c02bd2fc5b989a598d60982d994fa6d5ae134e
kramzer22 <markdavidpanganiban1218@hotmail.com>
2024-03-26 18:47:29 +0800

    add testing

--- index.js
+++ index.js
@@ -13,22 +13,16 @@ console.log(MONGODB_URI);
 
 // Test route Hello, world!
 app.get("/", (req, res) => {
-  mongoose.connection.on("connected", () => {
-    console.log("Connected to MongoDB");
-  });
-
-  mongoose.connection.on("error", (err) => {
-    console.error("MongoDB connection error:", err);
-  });
-
-  mongoose.connection.on("disconnected", () => {
-    console.log("Disconnected from MongoDB");
-  });
+  if (mongoose.connection.readyState === 1) {
+    console.log("Mongoose connected to MongoDB");
+  } else {
+    console.log("Mongoose connection failed");
+  }
 
   res.send("Hello, world!");
 });
 
-// mongoose.set("strictQuery", false);
+mongoose.set("strictQuery", false);
 mongoose.connect(MONGODB_URI);
 
 const PORT = process.env.PORT || 3001;
